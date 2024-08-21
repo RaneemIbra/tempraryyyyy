671f438853599d10317a11643afe2d1d832e6a8a
RaneemIbra <132738285+RaneemIbra@users.noreply.github.com>
2024-08-21 19:49:11 +0300

    hello10

--- src/main/java/com/example/khello/KhelloApplication.java
+++ src/main/java/com/example/khello/KhelloApplication.java
@@ -1,7 +1,4 @@
-package com.example.khello;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+package src.main.java.com.example.khello;
 
 //@SpringBootApplication
 public class KhelloApplication {
--- src/test/java/com/example/khello/KhelloApplicationTests.java
+++ src/test/java/com/example/khello/KhelloApplicationTests.java
@@ -1,8 +1,6 @@
-package com.example.khello;
+package src.test.java.com.example.khello;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //@SpringBootTest
